41508668c5f2f81600f9f7fa62889e39354012e4
ChristianCornelis <ccorneli@uoguelph.ca>
2020-02-23 17:59:17 -0500

    Updating monitoring

--- a2/monitor.py
+++ a2/monitor.py
@@ -4,6 +4,57 @@ import os
 import common.lib as lib
 import paramiko
 
+def monitor_ip(ip, user, key_env_var):
+    docker_images = lib.run_command('sudo docker images', ip, valid_user, os.getenv(key_env_var))
+
+    #if ec2-user fails, then try with `ubuntu` - default for ubuntu
+    if (docker_images['stderr'] == 'Error' and user == 'ec2-user'):
+        user = 'ubuntu'
+        docker_images = lib.run_command('sudo docker images', ip, user, os.getenv(key_env_var))
+
+    print('EC2 instance details for ' + ip + ':\n\n')
+    
+    print('Installed docker images on ' + ip + ':\n')
+
+    #retrieve installed docker images
+    if (docker_images['stderr'] == ''):
+        print(docker_images['stdout'])
+    
+    print('\n\nDocker images that were ran in the background:\n')
+    running_containers = lib.run_command('sudo docker ps -a', ip, user, os.getenv(key_env_var))
+    if (running_containers['stderr'] == ''):
+        print(running_containers['stdout'])
+    else:
+        print('ERROR: Could not retrieve images. Skipping this step for this IP address')
+        return
+    print('Output for running commands:\n')
+    docker_container_images = lib.run_command("sudo docker ps -a --format ‘{{.Image}}’", ip, user, os.getenv(key_env_var))
+    docker_container_ids = lib.run_command('sudo docker ps -aq', ip, user, os.getenv(key_env_var))
+    if (docker_container_images['stderr'] != ''):
+        print('ERROR: Could not retrieve all Docker containers with the following error:')
+        print(docker_container_images['stderr'])
+        print('Skipping this step for IP ' + ip)
+        return
+    elif (docker_container_ids['stderr'] != ''):
+        print('ERROR: Could not retrieve all Docker containers with the following error:')
+        print(docker_container_ids['stderr'])
+        print('Skipping this step for IP ' + ip)
+        return
+    else:
+        container_ids = docker_container_ids['stdout'].strip().split('\n')
+        container_images = docker_container_images['stdout'].strip().split('\n')
+        container_dict = {container_ids[i]: container_images[i] for i in range(len(container_ids))}
+
+        for container in container_dict.keys():
+            log_output = lib.run_command('sudo docker logs ' + container, ip, user, os.getenv(key_env_var))
+            if (log_output['stderr'] != ''):
+                print('ERROR: Could not retrieve log for Container with id ' + container + ' with output:')
+                print(log_output['stderr'])
+                return
+            else:
+                print('Running Docker ... ' + container_dict[container] + '\n')
+                print(log_output['stdout'])
+
 def monitor_all_aws_instances():
     ips = lib.get_ec2_ips()
     for image_id in ips.keys():
@@ -60,7 +111,10 @@ def monitor_all_aws_instances():
                     print(log_output['stdout'])
 
 
-    
+def monitor_all_azure_instances(ip_dict, ssh_dict):
+    ip_dict = lib.
+    for name in ip_dict.keys():
+        ip = ip_dict[]
 def monitor_all_instances():
     #parse json
     #get all instances and containers
